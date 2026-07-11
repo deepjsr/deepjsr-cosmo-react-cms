b0cf4b543a3902cc14a236f9bc763fad48af9c45
deepjsr <sarma.deepjyoti481@gmail.com>
2026-07-11 19:06:39 +0530

    fix:base URL with github

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import editableRegions from '@cloudcannon/editable-regions/astro-integration';
 
+const isGithub = process.env.GITHUB_ACTIONS === "true";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), editableRegions()],
